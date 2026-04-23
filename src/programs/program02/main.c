36ad1271d1afe850627472e812ad0d055f19cb22
Emma1514 <emma.zahrnhofer@sr.htlweiz.at>
2026-04-23 09:14:38 +0000

    Programm 4

--- src/programs/program02/main.c
+++ src/programs/program02/main.c
@@ -5,13 +5,15 @@
 #include <inttypes.h>
 #include <stdio.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   int16_t count = 0;
   // write your solution here
   int number = 0;
   printf("Geben Sie eine Zahl ein: ");
   scanf("%d", &number);
-  while (number <= 6){
+  while (number <= 6)
+  {
     printf("%d\n", number);
   }
   printf("Du hast %d Mal gültig gewürfelt.", count);
--- src/programs/program04/main.c
+++ src/programs/program04/main.c
@@ -1,18 +1,29 @@
-/// \file
-/// \author
-/// \date
+/// \file program04
+/// \author Emma Zahrnhofer
+/// \date 23.04.2026
 
 #include <inttypes.h>
 #include <stdio.h>
+#include <math.h>
 
 // TODO: your code goes here
+float compute_hypotenuse(float leg_a, float leg_b)
+{
+  float hypotenuse = 0;
+  hypotenuse = sqrt((leg_a * leg_a) + (leg_b * leg_b));
+  return hypotenuse;
+}
 
-int main(void) {
+int main(void)
+{
   double leg_a = 0, leg_b = 0;
 
+  printf("Länge der Kathete a: ");
   scanf("%lf", &leg_a);
-
-  printf("%f", compute_hypotenuse(leg_a, leg_b));
+  printf("Länge der Kathete b: ");
+  scanf("%lf", &leg_b);
+  
+  printf("Länge der Hypotenuse: %.2f\n", compute_hypotenuse(leg_a, leg_b));
 
   return 0;
 }
